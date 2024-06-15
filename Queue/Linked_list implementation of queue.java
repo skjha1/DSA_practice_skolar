c51097f8893612d46d455b938f076e3338a9bffd
Shivendra k jha <47923680+skjha1@users.noreply.github.com>
2024-06-15 17:56:29 +0530

    Create Linked_list implementation of queue.java

---
+++ Queue/Linked_list implementation of queue.java
@@ -0,0 +1,98 @@
+class Node {
+    int data;
+    Node next;
+
+    public Node(int data) {
+        this.data = data;
+        this.next = null;
+    }
+}
+
+public class Queue {
+    private Node front;
+    private Node rear;
+    private int size;
+
+    public Queue() {
+        this.front = null;
+        this.rear = null;
+        this.size = 0;
+    }
+
+    public boolean isEmpty() {
+        return front == null;
+    }
+
+    public void enqueue(int item) {
+        Node newNode = new Node(item);
+        if (rear != null) {
+            rear.next = newNode;
+        }
+        rear = newNode;
+        if (front == null) {
+            front = rear;
+        }
+        size++;
+        System.out.println("Enqueued: " + item);
+    }
+
+    public int dequeue() {
+        if (isEmpty()) {
+            System.out.println("Queue is empty");
+            return -1;
+        }
+        int item = front.data;
+        front = front.next;
+        if (front == null) {
+            rear = null;
+        }
+        size--;
+        System.out.println("Dequeued: " + item);
+        return item;
+    }
+
+    public int peek() {
+        if (isEmpty()) {
+            System.out.println("Queue is empty");
+            return -1;
+        }
+        return front.data;
+    }
+
+    public void display() {
+        if (isEmpty()) {
+            System.out.println("Queue is empty");
+            return;
+        }
+        Node current = front;
+        System.out.print("Queue elements: ");
+        while (current != null) {
+            System.out.print(current.data + " ");
+            current = current.next;
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        Queue queue = new Queue();
+        queue.enqueue(10);
+        queue.enqueue(20);
+        queue.enqueue(30);
+        queue.enqueue(40);
+        queue.enqueue(50);
+
+        queue.display();
+
+        queue.dequeue();
+        queue.dequeue();
+
+        queue.display();
+
+        queue.enqueue(60);
+        queue.enqueue(70);
+
+        queue.display();
+
+        System.out.println("Front element: " + queue.peek());
+    }
+}
